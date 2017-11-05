dd287677c6329c06ed7374f8e98f6a85a88093ff
JuanmaLambre <juanmlambre@gmail.com>
2017-11-05 18:21:39 -0300

    Include logic to App

--- src/main/java/ar/uba/fi/tdd/rulogic/App.java
+++ src/main/java/ar/uba/fi/tdd/rulogic/App.java
@@ -1,5 +1,12 @@
 package ar.uba.fi.tdd.rulogic;
 
+import ar.uba.fi.tdd.rulogic.DBParser;
+import ar.uba.fi.tdd.rulogic.ResourceReader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Console application.
  *
@@ -7,6 +14,22 @@ package ar.uba.fi.tdd.rulogic;
 public class App
 {
 	public static void main(String[] args) {
-		System.out.println("I shall answer all your questions!");
+		String dbContent = new ResourceReader("rules.db").read();
+		DBParser parser = new DBParser(dbContent);
+		DrKnow drKnow = new DrKnow(parser.getRules(), parser.getFacts());
+
+		try {
+			System.out.println("Starving minds, welcome to Dr. Know! Where fast food for thought"); 
+			System.out.println("is served up 24 hours a day, in 40,000 locations nationwide.");
+			System.out.println("Ask. Dr Know - there's nothing I don't!\n");
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			String query = br.readLine();
+			while (query != null) {
+				System.out.println(drKnow.ask(query));
+				query = br.readLine();
+			}
+		} catch (IOException e) {
+		}
     }
 }
